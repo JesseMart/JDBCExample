f968df565a2356548c152f6b781937bc3854eabf
Jesse Martinez <jessemartinez452@gmail.com>
2021-05-26 16:37:17 -0500

    Hid pw

--- src/main/java/com/qa/utils/DBconfig.java
+++ src/main/java/com/qa/utils/DBconfig.java
@@ -4,6 +4,6 @@ public class DBconfig {
 	//not ideal for security purposes
 public static String user = "root";
 	//pw is exposed
-public static String pw = "mJ051396";
+public static String pw = "hidden";
 public static String url = "jdbc:mysql://localhost:3306/jdbc_example";
 }
